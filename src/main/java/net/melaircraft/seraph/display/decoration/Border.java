1f8585b81d65dca308168992c2485557ea0787a9
Melair <melair@caldonia.net>
2017-09-28 22:06:18 +0100

    Add border decoration.

---
+++ src/main/java/net/melaircraft/seraph/display/decoration/Border.java
@@ -0,0 +1,79 @@
+package net.melaircraft.seraph.display.decoration;
+
+import net.melaircraft.seraph.display.CheckedDisplayable;
+import net.melaircraft.seraph.display.Displayable;
+import net.melaircraft.seraph.display.PixelColour;
+import net.melaircraft.seraph.display.layout.Window;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Border extends CheckedDisplayable {
+    private final Displayable window;
+
+    public Border(Displayable parent, PixelColour colour, Side... sides) {
+        List<Side> sideList = Arrays.asList(sides);
+
+        int offsetX = sideList.contains(Side.LEFT) ? 1 : 0;
+        int offsetY = sideList.contains(Side.TOP) ? 1 : 0;
+
+        int reductionW = sideList.contains(Side.LEFT) ? 1 : 0;
+        reductionW += sideList.contains(Side.RIGHT) ? 1 : 0;
+
+        int reductionH = sideList.contains(Side.TOP) ? 1 : 0;
+        reductionH += sideList.contains(Side.BOTTOM) ? 1 : 0;
+
+        this.window = new Window(parent, offsetX, offsetY, parent.getWidth() - reductionW, parent.getHeight() - reductionH);
+
+        if (sideList.contains(Side.TOP)) {
+            for (int x = 0; x < parent.getWidth(); x++) {
+                parent.setPixel(x, 0, colour);
+            }
+        }
+
+        if (sideList.contains(Side.BOTTOM)) {
+            for (int x = 0; x < parent.getWidth(); x++) {
+                parent.setPixel(x, (parent.getHeight() - 1), colour);
+            }
+        }
+
+        if (sideList.contains(Side.LEFT)) {
+            for (int y = 0; y < parent.getHeight(); y++) {
+                parent.setPixel(0, y, colour);
+            }
+        }
+
+        if (sideList.contains(Side.RIGHT)) {
+            for (int y = 0; y < parent.getHeight(); y++) {
+                parent.setPixel((parent.getWidth() - 1), y, colour);
+            }
+        }
+    }
+
+    @Override
+    protected void setActualPixel(int x, int y, int r, int g, int b) {
+        window.setPixel(x, y, r, g, b);
+    }
+
+    @Override
+    protected PixelColour getActualPixel(int x, int y) {
+        return window.getPixel(x, y);
+    }
+
+    @Override
+    public int getWidth() {
+        return window.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return window.getHeight();
+    }
+
+    enum Side {
+        TOP,
+        LEFT,
+        RIGHT,
+        BOTTOM
+    }
+}
---
+++ src/test/java/net/melaircraft/seraph/display/decoration/BorderTest.java
@@ -0,0 +1,46 @@
+package net.melaircraft.seraph.display.decoration;
+
+import net.melaircraft.seraph.display.Displayable;
+import net.melaircraft.seraph.display.PixelColour;
+import net.melaircraft.seraph.display.output.Buffer;
+import net.melaircraft.seraph.display.output.Null;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BorderTest {
+    Displayable store = new Buffer(new Null(6, 6));
+    PixelColour colour = new PixelColour(255, 0, 0);
+
+    @Test
+    public void testBordersOnAllSidesDimensions() {
+        Displayable border = new Border(store, colour, Border.Side.TOP, Border.Side.LEFT, Border.Side.RIGHT, Border.Side.BOTTOM);
+
+        assertEquals(store.getWidth() - 2, border.getWidth());
+        assertEquals(store.getHeight() - 2, border.getHeight());
+    }
+
+    @Test
+    public void testPixelWriteThrough() {
+        Displayable border = new Border(store, colour, Border.Side.TOP, Border.Side.LEFT, Border.Side.RIGHT, Border.Side.BOTTOM);
+        PixelColour dot = new PixelColour(0, 255, 0);
+
+        border.setPixel(0, 0, dot);
+        border.setPixel(3, 3, dot);
+
+        assertEquals(dot, store.getPixel(1, 1));
+        assertEquals(dot, store.getPixel(4, 4));
+        assertEquals(dot, border.getPixel(0, 0));
+        assertEquals(dot, border.getPixel(3, 3));
+    }
+
+    @Test
+    public void testBorders() {
+        Displayable border = new Border(store, colour, Border.Side.TOP, Border.Side.LEFT, Border.Side.RIGHT, Border.Side.BOTTOM);
+
+        assertEquals(colour, store.getPixel(0, 1));
+        assertEquals(colour, store.getPixel(1, 0));
+        assertEquals(colour, store.getPixel(0, 5));
+        assertEquals(colour, store.getPixel(5, 0));
+    }
+}
