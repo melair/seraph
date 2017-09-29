0640180ec018f9a1d314d74fb358701dcfa2ff19
Melair <melair@caldonia.net>
2017-09-29 12:39:07 +0100

    Add greyscale filter, closed #6.

---
+++ src/main/java/net/melaircraft/seraph/display/filter/Greyscale.java
@@ -0,0 +1,25 @@
+package net.melaircraft.seraph.display.filter;
+
+import net.melaircraft.seraph.display.Displayable;
+import net.melaircraft.seraph.display.PixelColour;
+import net.melaircraft.seraph.display.output.Buffer;
+
+public class Greyscale extends Buffer {
+    private final Displayable parent;
+
+    public Greyscale(Displayable parent) {
+        super(parent);
+        this.parent = parent;
+    }
+
+    @Override
+    protected void setActualPixel(int x, int y, int r, int g, int b) {
+        super.setActualPixel(x, y, r, g, b);
+        setParentPixelScaled(x, y, r, g, b);
+    }
+
+    private void setParentPixelScaled(int x, int y, int r, int g, int b) {
+        int grey = (int) Math.round((0.2125 * r) + (0.7154 * g) + (0.0721 * b));
+        parent.setPixel(x, y, grey, grey, grey);
+    }
+}
---
+++ src/test/java/net/melaircraft/seraph/display/filter/GreyscaleTest.java
@@ -0,0 +1,52 @@
+package net.melaircraft.seraph.display.filter;
+
+import net.melaircraft.seraph.display.Displayable;
+import net.melaircraft.seraph.display.output.Buffer;
+import net.melaircraft.seraph.display.output.Null;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GreyscaleTest {
+    Displayable store = new Buffer(new Null(4, 2));
+
+    @Test
+    public void testDimensionsPassThrough() {
+        Displayable greyscale = new Greyscale(store);
+
+        assertEquals(store.getWidth(), greyscale.getWidth());
+        assertEquals(store.getHeight(), greyscale.getHeight());
+    }
+
+    @Test
+    public void testGreyscaleWriteThroughBrightWhite() {
+        Displayable greyscale = new Greyscale(store);
+
+        greyscale.setPixel(0, 0, 255, 255, 255);
+
+        assertEquals(255, store.getPixel(0, 0).getRed());
+        assertEquals(255, greyscale.getPixel(0, 0).getRed());
+
+        assertEquals(255, store.getPixel(0, 0).getGreen());
+        assertEquals(255, greyscale.getPixel(0, 0).getGreen());
+
+        assertEquals(255, store.getPixel(0, 0).getBlue());
+        assertEquals(255, greyscale.getPixel(0, 0).getBlue());
+    }
+
+    @Test
+    public void testGreyscaleWriteThroughDimRed() {
+        Displayable greyscale = new Greyscale(store);
+
+        greyscale.setPixel(0, 0, 60, 0, 0);
+
+        assertEquals(13, store.getPixel(0, 0).getRed());
+        assertEquals(60, greyscale.getPixel(0, 0).getRed());
+
+        assertEquals(13, store.getPixel(0, 0).getGreen());
+        assertEquals(0, greyscale.getPixel(0, 0).getGreen());
+
+        assertEquals(13, store.getPixel(0, 0).getBlue());
+        assertEquals(0, greyscale.getPixel(0, 0).getBlue());
+    }
+}
