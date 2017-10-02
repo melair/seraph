c3f99abcf03a32434646813b7f837e5237eccd73
Melair <melair@caldonia.net>
2017-10-02 12:24:10 +0100

    Fix failing test in Mask by refactoring to use composition rather then inheritance.

--- display-library/src/main/java/net/melaircraft/seraph/display/filter/Mask.java
+++ display-library/src/main/java/net/melaircraft/seraph/display/filter/Mask.java
@@ -3,36 +3,67 @@ package net.melaircraft.seraph.display.filter;
 import net.melaircraft.seraph.display.Displayable;
 import net.melaircraft.seraph.display.PixelColour;
 import net.melaircraft.seraph.display.buffer.Buffer;
+import net.melaircraft.seraph.display.exception.InvalidPixelColourException;
+import net.melaircraft.seraph.display.exception.NonExistentPixelException;
 import net.melaircraft.seraph.display.output.Null;
 
-public class Mask extends Buffer {
+public class Mask implements Displayable {
     private final Displayable mask;
+    private final Displayable northBuffer;
+    private final Displayable parent;
 
     public Mask(Displayable parent) {
-        super(parent);
-
+        northBuffer = new Buffer(new Null(parent.getWidth(), parent.getHeight()));
         mask = new MaskLayer(new Null(parent.getWidth(), parent.getHeight()));
+        this.parent = parent;
     }
 
     public Displayable getMask() {
         return mask;
     }
 
-    private void update(int x, int y) {
-        PixelColour colour = getActualPixel(x, y);
-        setActualPixel(x, y, colour.getRed(), colour.getGreen(), colour.getBlue());
+    private void maskUpdate(int x, int y) {
+        outputPixelToParent(x, y);
     }
-    
+
+
     @Override
-    protected void setActualPixel(int x, int y, int r, int g, int b) {
+    public int getWidth() {
+        return parent.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return parent.getHeight();
+    }
+
+    @Override
+    public void setPixel(int x, int y, int r, int g, int b) throws NonExistentPixelException, InvalidPixelColourException {
+        northBuffer.setPixel(x, y, r, g, b);
+        outputPixelToParent(x, y);
+    }
+
+    private void outputPixelToParent(int x, int y) {
+        PixelColour northPixel = northBuffer.getPixel(x, y);
+
+        int r = northPixel.getRed();
+        int g = northPixel.getBlue();
+        int b = northPixel.getBlue();
+
         double intensity = ((0.2125 * r) + (0.7154 * g) + (0.0721 * b)) / 255;
+
         PixelColour maskPixel = mask.getPixel(x, y);
 
         r = (int) Math.round(intensity * maskPixel.getRed());
         g = (int) Math.round(intensity * maskPixel.getGreen());
         b = (int) Math.round(intensity * maskPixel.getBlue());
 
-        super.setActualPixel(x, y, r, g, b);
+        parent.setPixel(x, y, r, g, b);
+    }
+
+    @Override
+    public PixelColour getPixel(int x, int y) throws NonExistentPixelException {
+        return northBuffer.getPixel(x, y);
     }
 
     class MaskLayer extends Buffer {
@@ -43,7 +74,7 @@ public class Mask extends Buffer {
         @Override
         protected void setActualPixel(int x, int y, int r, int g, int b) {
             super.setActualPixel(x, y, r, g, b);
-            update(x, y);
+            maskUpdate(x, y);
         }
     }
 }
