98575c7cca0362924045048236074b67fe166b4e
Melair <melair@caldonia.net>
2017-10-11 21:09:22 +0100

    Remove initialising buffer and commit missed pixel colour test.

---
+++ display-library/src/test/java/net/melaircraft/seraph/display/PixelColourTest.java
@@ -0,0 +1,59 @@
+package net.melaircraft.seraph.display;
+
+import net.melaircraft.seraph.display.exception.InvalidPixelColourException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PixelColourTest {
+    @Test(expected = InvalidPixelColourException.class)
+    public void testRedPixelConstructionLower() {
+        new PixelColour(-1, 0, 0);
+    }
+
+    @Test(expected = InvalidPixelColourException.class)
+    public void testRedPixelConstructionHigher() {
+        new PixelColour(256, 0, 0);
+    }
+
+    @Test(expected = InvalidPixelColourException.class)
+    public void testGreenPixelConstructionLower() {
+        new PixelColour(0, -1, 0);
+    }
+
+    @Test(expected = InvalidPixelColourException.class)
+    public void testGreenPixelConstructionHigher() {
+        new PixelColour( 0, 256, 0);
+    }
+
+    @Test(expected = InvalidPixelColourException.class)
+    public void testBluePixelConstructionLower() {
+        new PixelColour(0, 0, -1);
+    }
+
+    @Test(expected = InvalidPixelColourException.class)
+    public void testBluePixelConstructionHigher() {
+        new PixelColour( 0, 0, 256);
+    }
+
+    @Test
+    public void testMultiplicationFactor() {
+        PixelColour original = new PixelColour(50, 100, 150);
+        PixelColour newColour = original.multiply(0.5);
+
+        assertEquals(25, newColour.getRed());
+        assertEquals(50, newColour.getGreen());
+        assertEquals(75, newColour.getBlue());
+    }
+
+    @Test
+    public void testMultiplicationColour() {
+        PixelColour original = new PixelColour(64, 64, 64);
+        PixelColour mixedIn = new PixelColour(127, 127, 127);
+        PixelColour newColour = original.multiply(mixedIn);
+
+        assertEquals(32, newColour.getRed());
+        assertEquals(32, newColour.getGreen());
+        assertEquals(32, newColour.getBlue());
+    }
+}
