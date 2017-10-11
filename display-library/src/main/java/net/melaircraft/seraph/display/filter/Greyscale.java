3fdebfb3cbc7f98e513579dcc82c50dd3293f03b
Melair <melair@caldonia.net>
2017-10-11 20:52:44 +0100

    Stop greyscale using buffer, reduce to DestinationDisplay interface.

--- display-library/src/main/java/net/melaircraft/seraph/display/filter/Greyscale.java
+++ display-library/src/main/java/net/melaircraft/seraph/display/filter/Greyscale.java
@@ -2,23 +2,27 @@ package net.melaircraft.seraph.display.filter;
 
 import net.melaircraft.seraph.display.DestinationDisplay;
 import net.melaircraft.seraph.display.PixelColour;
-import net.melaircraft.seraph.display.buffer.Buffer;
+import net.melaircraft.seraph.display.exception.NonExistentPixelException;
 
-public class Greyscale extends Buffer {
+public class Greyscale implements DestinationDisplay {
     private final DestinationDisplay parent;
 
     public Greyscale(DestinationDisplay parent) {
-        super(parent);
         this.parent = parent;
     }
 
     @Override
-    public void setActualPixel(int x, int y, PixelColour pixelColour) {
-        super.setActualPixel(x, y, pixelColour);
-        setParentPixelScaled(x, y, pixelColour);
+    public void setPixel(int x, int y, PixelColour pixelColour) throws NonExistentPixelException {
+        parent.setPixel(x, y, pixelColour.greyscale());
     }
 
-    private void setParentPixelScaled(int x, int y, PixelColour pixelColour) {
-        parent.setPixel(x, y, pixelColour.greyscale());
+    @Override
+    public int getWidth() {
+        return parent.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return parent.getHeight();
     }
 }
