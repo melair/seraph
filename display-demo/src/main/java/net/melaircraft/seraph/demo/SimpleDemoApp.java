b2595040d4cb4730e953085214f2122af8ada027
Melair <melair@caldonia.net>
2017-10-12 20:37:10 +0100

    Expand demo to use new scrolling text.

--- display-demo/src/main/java/net/melaircraft/seraph/demo/SimpleDemoApp.java
+++ display-demo/src/main/java/net/melaircraft/seraph/demo/SimpleDemoApp.java
@@ -2,6 +2,8 @@ package net.melaircraft.seraph.demo;
 
 import net.melaircraft.seraph.display.DestinationDisplay;
 import net.melaircraft.seraph.display.PixelColour;
+import net.melaircraft.seraph.display.buffer.Buffer;
+import net.melaircraft.seraph.display.component.Canvas;
 import net.melaircraft.seraph.display.component.Text;
 import net.melaircraft.seraph.display.decoration.Border;
 import net.melaircraft.seraph.display.decoration.Rainbow;
@@ -46,8 +48,87 @@ public class SimpleDemoApp {
         DestinationDisplay bottom2 = stack2.addLayer();
 
         BitmapFont bitmapFont = BDFFontFactory.load(new FileInputStream("contrib/gohufont-11.bdf"));
-        Text text = new Text(top2, bitmapFont, PixelColour.WHITE, Text.Justification.CENTER, Text.Alignment.BOTTOM, getTime());
-        Text text2 = new Text(bottom2, bitmapFont, PixelColour.WHITE, Text.Justification.CENTER, Text.Alignment.TOP, "Time is");
+        Canvas canvas = new Canvas(top2, Canvas.Direction.LEFT, Canvas.Alignment.MIDDLE, Canvas.Alignment.TOP);
+        canvas.append(new Text(bitmapFont, PixelColour.WHITE, "I am the very model of a modern Major-General / I've information vegetable, animal, and mineral"));
+
+        Canvas canvas2 = new Canvas(bottom2, Canvas.Direction.RIGHT, Canvas.Alignment.MIDDLE, Canvas.Alignment.BOTTOM);
+        Buffer buffer = new Buffer(11, 9);
+
+        buffer.setPixel(2, 0, PixelColour.WHITE);
+        buffer.setPixel(3, 0, PixelColour.WHITE);
+        buffer.setPixel(4, 0, PixelColour.WHITE);
+        buffer.setPixel(5, 0, PixelColour.WHITE);
+        buffer.setPixel(6, 0, PixelColour.WHITE);
+
+        buffer.setPixel(1, 1, PixelColour.WHITE);
+        buffer.setPixel(2, 1, PixelColour.WHITE);
+        buffer.setPixel(3, 1, PixelColour.WHITE);
+        buffer.setPixel(5, 1, PixelColour.WHITE);
+        buffer.setPixel(6, 1, PixelColour.WHITE);
+        buffer.setPixel(7, 1, PixelColour.WHITE);
+
+        buffer.setPixel(0, 2, PixelColour.WHITE);
+        buffer.setPixel(1, 2, PixelColour.WHITE);
+        buffer.setPixel(2, 2, PixelColour.WHITE);
+        buffer.setPixel(3, 2, PixelColour.WHITE);
+        buffer.setPixel(5, 2, PixelColour.WHITE);
+        buffer.setPixel(6, 2, PixelColour.WHITE);
+        buffer.setPixel(7, 2, PixelColour.WHITE);
+        buffer.setPixel(8, 2, PixelColour.WHITE);
+
+        buffer.setPixel(0, 3, PixelColour.WHITE);
+        buffer.setPixel(1, 3, PixelColour.WHITE);
+        buffer.setPixel(2, 3, PixelColour.WHITE);
+        buffer.setPixel(3, 3, PixelColour.WHITE);
+        buffer.setPixel(5, 3, PixelColour.WHITE);
+        buffer.setPixel(6, 3, PixelColour.WHITE);
+        buffer.setPixel(7, 3, PixelColour.WHITE);
+        buffer.setPixel(8, 3, PixelColour.WHITE);
+
+        buffer.setPixel(0, 4, PixelColour.WHITE);
+        buffer.setPixel(1, 4, PixelColour.WHITE);
+        buffer.setPixel(2, 4, PixelColour.WHITE);
+        buffer.setPixel(3, 4, PixelColour.WHITE);
+        buffer.setPixel(7, 4, PixelColour.WHITE);
+        buffer.setPixel(8, 4, PixelColour.WHITE);
+
+        buffer.setPixel(0, 5, PixelColour.WHITE);
+        buffer.setPixel(1, 5, PixelColour.WHITE);
+        buffer.setPixel(2, 5, PixelColour.WHITE);
+        buffer.setPixel(3, 5, PixelColour.WHITE);
+        buffer.setPixel(4, 5, PixelColour.WHITE);
+        buffer.setPixel(5, 5, PixelColour.WHITE);
+        buffer.setPixel(6, 5, PixelColour.WHITE);
+        buffer.setPixel(7, 5, PixelColour.WHITE);
+        buffer.setPixel(8, 5, PixelColour.WHITE);
+
+        buffer.setPixel(0, 6, PixelColour.WHITE);
+        buffer.setPixel(1, 6, PixelColour.WHITE);
+        buffer.setPixel(2, 6, PixelColour.WHITE);
+        buffer.setPixel(3, 6, PixelColour.WHITE);
+        buffer.setPixel(4, 6, PixelColour.WHITE);
+        buffer.setPixel(5, 6, PixelColour.WHITE);
+        buffer.setPixel(6, 6, PixelColour.WHITE);
+        buffer.setPixel(7, 6, PixelColour.WHITE);
+        buffer.setPixel(8, 6, PixelColour.WHITE);
+
+        buffer.setPixel(1, 7, PixelColour.WHITE);
+        buffer.setPixel(2, 7, PixelColour.WHITE);
+        buffer.setPixel(3, 7, PixelColour.WHITE);
+        buffer.setPixel(4, 7, PixelColour.WHITE);
+        buffer.setPixel(5, 7, PixelColour.WHITE);
+        buffer.setPixel(6, 7, PixelColour.WHITE);
+        buffer.setPixel(7, 7, PixelColour.WHITE);
+
+        buffer.setPixel(2, 8, PixelColour.WHITE);
+        buffer.setPixel(3, 8, PixelColour.WHITE);
+        buffer.setPixel(4, 8, PixelColour.WHITE);
+        buffer.setPixel(5, 8, PixelColour.WHITE);
+        buffer.setPixel(6, 8, PixelColour.WHITE);
+
+        canvas2.append(new Buffer(0, 11));
+        canvas2.append(buffer);
+        canvas2.append(new Text(bitmapFont, PixelColour.WHITE, "--:--:--"));
 
         DestinationDisplay bottom = stack.addLayer();
         Book book = new Book(bottom, true);
@@ -66,7 +147,9 @@ public class SimpleDemoApp {
 
         scheduler.scheduleAtFixedRate(seraphProtocol, 0, 50, TimeUnit.MILLISECONDS);
 
-        scheduler.scheduleAtFixedRate(() -> text.setText(getTime()), 1, 1, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(canvas, 0, 100, TimeUnit.MILLISECONDS);
+        scheduler.scheduleAtFixedRate(canvas2, 0, 50, TimeUnit.MILLISECONDS);
+        scheduler.scheduleAtFixedRate(() -> canvas2.replace(2, new Text(bitmapFont, PixelColour.WHITE, getTime())), 1, 1, TimeUnit.SECONDS);
         scheduler.scheduleAtFixedRate(snow, 0, 300, TimeUnit.MILLISECONDS);
         scheduler.scheduleAtFixedRate(matrix, 0, 50, TimeUnit.MILLISECONDS);
         scheduler.scheduleAtFixedRate(book::nextPage, 5000, 5000, TimeUnit.MILLISECONDS);
