655a1873e30560a39072a4395e94ed81ba8cebc6
Melair <melair@caldonia.net>
2017-10-12 12:39:33 +0100

    Rework text to be a source display.

--- display-library/src/main/java/net/melaircraft/seraph/display/component/Text.java
+++ display-library/src/main/java/net/melaircraft/seraph/display/component/Text.java
@@ -1,78 +1,42 @@
 package net.melaircraft.seraph.display.component;
 
-import net.melaircraft.seraph.display.DestinationDisplay;
+import net.melaircraft.seraph.display.CheckedSourceDisplay;
 import net.melaircraft.seraph.display.PixelColour;
-import net.melaircraft.seraph.display.buffer.DeltaBuffer;
+import net.melaircraft.seraph.display.SourceDisplay;
+import net.melaircraft.seraph.display.exception.NonExistentPixelException;
 import net.melaircraft.seraph.fonts.BitmapFont;
 import net.melaircraft.seraph.fonts.FontOutput;
 
-public class Text {
-    private final DestinationDisplay destination;
-    private final BitmapFont bitmapFont;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Text implements CheckedSourceDisplay {
     private final PixelColour colour;
-    private final Justification justification;
-    private final Alignment alignment;
-    private final DeltaBuffer deltaBuffer;
+    private final FontOutput fontOutput;
 
-    public Text(DestinationDisplay destination, BitmapFont bitmapFont, PixelColour colour, Justification justification, Alignment alignment, String initialContent) {
-        this.destination = destination;
-        this.bitmapFont = bitmapFont;
+    public Text(BitmapFont bitmapFont, PixelColour colour, String content) {
         this.colour = colour;
-        this.justification = justification;
-        this.alignment = alignment;
-        this.deltaBuffer = new DeltaBuffer(destination);
-
-        setText(initialContent);
+        this.fontOutput = bitmapFont.render(content);
     }
 
-    public void setText(String content) {
-        FontOutput fontOutput = bitmapFont.render(content);
-
-        deltaBuffer.clearCurrent();
-
-        int dX = 0;
-        int dY = 0;
-
-        switch (justification) {
-            case CENTER:
-                dX = (destination.getWidth() - fontOutput.getWidth()) / 2;
-                break;
-            case RIGHT:
-                dX = destination.getWidth() - fontOutput.getWidth();
-                break;
-        }
-
-        switch (alignment) {
-            case MIDDLE:
-                dY = (destination.getHeight() - fontOutput.getHeight()) / 2;
-                break;
-            case BOTTOM:
-                dY = destination.getHeight() - fontOutput.getHeight();
-                break;
-        }
-
-        boolean[][] bitmap = fontOutput.getBitmap();
-
-        for (int x = 0; x < fontOutput.getWidth(); x++) {
-            for (int y = 0; y < fontOutput.getHeight(); y++) {
-                if (bitmap[x][y]) {
-                    deltaBuffer.setPixel(x + dX, y + dY, colour);
-                }
-            }
-        }
-
-        deltaBuffer.sendDelta();
+    @Override
+    public PixelColour getActualPixel(int x, int y) {
+        return fontOutput.getBitmap()[x][y] ? colour : PixelColour.BLACK;
     }
 
-    public enum Justification {
-        LEFT,
-        CENTER,
-        RIGHT
+    @Override
+    public Collection<DisplayCallback> getCallbacks() {
+        return Collections.emptySet();
     }
 
-    public enum Alignment {
-        TOP,
-        MIDDLE,
-        BOTTOM
+    @Override
+    public int getWidth() {
+        return fontOutput.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return fontOutput.getHeight();
     }
 }
--- display-library/src/test/java/net/melaircraft/seraph/display/component/TextTest.java
+++ display-library/src/test/java/net/melaircraft/seraph/display/component/TextTest.java
@@ -1,7 +1,6 @@
 package net.melaircraft.seraph.display.component;
 
 import net.melaircraft.seraph.display.PixelColour;
-import net.melaircraft.seraph.display.buffer.Buffer;
 import net.melaircraft.seraph.fonts.BDFFontFactory;
 import net.melaircraft.seraph.fonts.BitmapFont;
 import org.junit.Test;
@@ -14,55 +13,17 @@ import static org.junit.Assert.assertEquals;
 public class TextTest {
     @Test
     public void testTextBeingSetTopLeft() throws IOException {
-        Buffer store = new Buffer(32, 32);
-
         InputStream inputStream = TextTest.class.getResourceAsStream("/testfont.bdf");
         BitmapFont font = BDFFontFactory.load(inputStream);
         inputStream.close();
 
         PixelColour colour = PixelColour.RED;
 
-        Text text = new Text(store, font, colour, Text.Justification.LEFT, Text.Alignment.TOP, "A");
+        Text text = new Text(font, colour, "A");
 
-        assertEquals(PixelColour.RED, store.getPixel(3, 4));
-        assertEquals(PixelColour.RED, store.getPixel(4, 4));
-        assertEquals(PixelColour.RED, store.getPixel(2, 5));
-        assertEquals(PixelColour.RED, store.getPixel(5, 5));
-    }
-
-    @Test
-    public void testTextBeingSetCentreMiddle() throws IOException {
-        Buffer store = new Buffer(32, 32);
-
-        InputStream inputStream = TextTest.class.getResourceAsStream("/testfont.bdf");
-        BitmapFont font = BDFFontFactory.load(inputStream);
-        inputStream.close();
-
-        PixelColour colour = PixelColour.RED;
-
-        Text text = new Text(store, font, colour, Text.Justification.CENTER, Text.Alignment.MIDDLE, "A");
-
-        assertEquals(PixelColour.RED, store.getPixel(15, 12));
-        assertEquals(PixelColour.RED, store.getPixel(16, 12));
-        assertEquals(PixelColour.RED, store.getPixel(14, 13));
-        assertEquals(PixelColour.RED, store.getPixel(17, 13));
-    }
-
-    @Test
-    public void testTextBeingSetBottomRight() throws IOException {
-        Buffer store = new Buffer(32, 32);
-
-        InputStream inputStream = TextTest.class.getResourceAsStream("/testfont.bdf");
-        BitmapFont font = BDFFontFactory.load(inputStream);
-        inputStream.close();
-
-        PixelColour colour = PixelColour.RED;
-
-        Text text = new Text(store, font, colour, Text.Justification.RIGHT, Text.Alignment.BOTTOM, "A");
-
-        assertEquals(PixelColour.RED, store.getPixel(27, 20));
-        assertEquals(PixelColour.RED, store.getPixel(28, 20));
-        assertEquals(PixelColour.RED, store.getPixel(26, 21));
-        assertEquals(PixelColour.RED, store.getPixel(29, 21));
+        assertEquals(PixelColour.RED, text.getPixel(3, 4));
+        assertEquals(PixelColour.RED, text.getPixel(4, 4));
+        assertEquals(PixelColour.RED, text.getPixel(2, 5));
+        assertEquals(PixelColour.RED, text.getPixel(5, 5));
     }
 }
