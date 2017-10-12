05a9cf050c22dab64b17b400463c204c0e444eb4
Melair <melair@caldonia.net>
2017-10-12 13:31:42 +0100

    Fill out more of ticker.

--- display-library/src/main/java/net/melaircraft/seraph/display/component/Ticker.java
+++ display-library/src/main/java/net/melaircraft/seraph/display/component/Ticker.java
@@ -1,6 +1,7 @@
 package net.melaircraft.seraph.display.component;
 
 import net.melaircraft.seraph.display.DestinationDisplay;
+import net.melaircraft.seraph.display.HasSize;
 import net.melaircraft.seraph.display.PixelColour;
 import net.melaircraft.seraph.display.SourceDisplay;
 import net.melaircraft.seraph.display.buffer.DeltaBuffer;
@@ -15,14 +16,25 @@ public class Ticker implements SourceDisplay.DisplayCallback, Runnable {
     private final Alignment groupAlignment;
     private final DeltaBuffer deltaBuffer;
 
+    private final int parentWidth;
+
+    private int currentWidth = 0;
+    private int currentHeight = 0;
+
+    private int currentPos = 0;
+
     private final List<SourceDisplay> displays = new ArrayList<>();
 
     public Ticker(DestinationDisplay destinationDisplay, Direction scrollDirection, Alignment itemAlignment, Alignment groupAlignment) {
+        this.parentWidth = destinationDisplay.getWidth();
+
         this.scrollDirection = scrollDirection;
         this.justification = Justification.LEFT;
         this.itemAlignment = itemAlignment;
         this.groupAlignment = groupAlignment;
         this.deltaBuffer = new DeltaBuffer(destinationDisplay);
+
+        recalculateSize();
     }
 
     public Ticker(DestinationDisplay destinationDisplay, Justification justification, Alignment itemAlignment, Alignment groupAlignment) {
@@ -31,41 +43,74 @@ public class Ticker implements SourceDisplay.DisplayCallback, Runnable {
         this.itemAlignment = itemAlignment;
         this.groupAlignment = groupAlignment;
         this.deltaBuffer = new DeltaBuffer(destinationDisplay);
+
+        recalculateSize();
     }
 
     public void append(SourceDisplay source) {
         displays.add(source);
-        redraw();
+        recalculateSize();
     }
 
     public void insert(int position, SourceDisplay source) {
         displays.add(position, source);
-        redraw();
+        recalculateSize();
     }
 
     public void replace(int position, SourceDisplay source) {
         displays.remove(position);
         displays.add(position, source);
-        redraw();
+        recalculateSize();
     }
 
     public void remove(SourceDisplay source) {
         displays.remove(source);
-        redraw();
+        recalculateSize();
     }
 
-    private void redraw() {
-
+    private void recalculateSize() {
+        currentHeight = displays.stream().mapToInt(HasSize::getHeight).max().orElse(0);
+        currentWidth = displays.stream().mapToInt(HasSize::getWidth).sum();
     }
 
     @Override
     public void notification(SourceDisplay display, int x, int y, PixelColour colour) {
-
+        redraw();
     }
 
     @Override
     public void run() {
+        switch (scrollDirection) {
+            case LEFT:
+                currentPos++;
+
+                if (currentPos > currentWidth + parentWidth) {
+                    currentPos = 0 - parentWidth;
+                }
+                break;
+
+            case RIGHT:
+                currentPos--;
+
+                if (currentPos < -parentWidth) {
+                    currentPos = currentWidth + parentWidth;
+                }
+                break;
+        }
+
+        if (scrollDirection == Direction.NONE) {
+            return;
+        }
+
         redraw();
+    }
+
+    private void redraw() {
+        deltaBuffer.clearCurrent();
+
+        // Calculate New Window
+
+        deltaBuffer.sendDelta();
     }
 
     public enum Justification {
