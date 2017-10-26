27286389236f676660fe9bd073e0c3b0d9985762
Melair <melair@caldonia.net>
2017-10-26 19:34:21 +0100

    Complete rest interface for allowing putting of data into the data manager.

--- display-server/src/main/java/net/melaircraft/seraph/server/rest/endpoints/Data.java
+++ display-server/src/main/java/net/melaircraft/seraph/server/rest/endpoints/Data.java
@@ -2,9 +2,15 @@ package net.melaircraft.seraph.server.rest.endpoints;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import net.melaircraft.seraph.server.data.DataManager;
 import net.melaircraft.seraph.server.data.Entry;
+import net.melaircraft.seraph.server.data.value.BooleanValue;
+import net.melaircraft.seraph.server.data.value.NumberValue;
 import net.melaircraft.seraph.server.data.value.StringValue;
 import net.melaircraft.seraph.server.data.value.Value;
 
@@ -15,11 +21,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import java.util.function.BiConsumer;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/data")
 public class Data {
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private final JsonParser jsonParser = new JsonParser();
     private final DataManager dataManager;
     private final boolean allowWrite;
 
@@ -52,14 +60,55 @@ public class Data {
     @Consumes("application/json")
     public Response put(@PathParam("name") String name, String body) {
         if (!allowWrite) {
-            JsonObject message = new JsonObject();
-            message.addProperty("message", "Writes are not enabled.");
-            JsonObject status = new JsonObject();
-            status.add("error", message);
-            return Response.status(Response.Status.METHOD_NOT_ALLOWED).entity(gson.toJson(status)).type("application/json").build();
+            return generateError(Response.Status.METHOD_NOT_ALLOWED,"Writes are not enabled.");
         }
 
-        return Response.status(Response.Status.NOT_IMPLEMENTED).build();
+        JsonObject jsonObject;
+
+        try {
+            jsonObject = jsonParser.parse(body).getAsJsonObject();
+        } catch (JsonIOException | IllegalStateException e) {
+            return generateError(Response.Status.BAD_REQUEST, "JSON provided is not valid.");
+        }
+
+        Map<String, Value> values = new HashMap<>();
+
+        for (String key : jsonObject.keySet()) {
+            JsonElement element = jsonObject.get(key);
+
+            if (!element.isJsonPrimitive()) {
+                return generateError(Response.Status.BAD_REQUEST, "Data must only contain primitive values.");
+            }
+
+            JsonPrimitive primitive = (JsonPrimitive) element;
+
+            if (primitive.isString()) {
+                values.put(key, new StringValue(primitive.getAsString()));
+            } else if (primitive.isBoolean()) {
+                values.put(key, new BooleanValue(primitive.getAsBoolean()));
+            } else if (primitive.isNumber()) {
+                values.put(key, new NumberValue(primitive.getAsNumber().doubleValue()));
+            }
+        }
+
+        Entry entry = dataManager.get(name);
+        values.forEach(entry::add);
+
+        return generateStatus(Response.Status.OK, "Data accepted.");
+    }
+
+    private Response generateError(Response.Status responseStatus, String messageText) {
+        JsonObject message = new JsonObject();
+        message.addProperty("message", messageText);
+        JsonObject status = new JsonObject();
+        status.add("error", message);
+        return Response.status(responseStatus).entity(gson.toJson(status)).type("application/json").build();
+    }
+
+    private Response generateStatus(Response.Status responseStatus, String statusText) {
+        JsonObject message = new JsonObject();
+        message.addProperty("status", statusText);
+        return Response.status(responseStatus).entity(gson.toJson(message)).type("application/json").build();
     }
 
     private void addToJson(JsonObject jsonObject, String key, Value value) {
--- display-server/src/test/java/net/melaircraft/seraph/server/rest/endpoints/DataTest.java
+++ display-server/src/test/java/net/melaircraft/seraph/server/rest/endpoints/DataTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.util.Collections;
 import java.util.Set;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class DataTest extends RestTest {
@@ -35,5 +36,47 @@ public class DataTest extends RestTest {
             .body("test.string", equalTo("string"))
             .body("test.value", equalTo(3.142F))
             .body("test.boolean", equalTo(true));
+    }
+
+    @Test
+    public void testWriteFailsDueToInvalidJSON() {
+        request
+            .accept("application/json")
+            .contentType("application/json")
+            .body("ABC")
+            .put("/data/name")
+        .then()
+            .statusCode(400)
+            .body("error.message", equalTo("JSON provided is not valid."));
+    }
+
+    @Test
+    public void testWriteFailsDueToNestedObject() {
+        request
+            .accept("application/json")
+            .contentType("application/json")
+            .body("{ \"a\": 2, \"b\": {} }")
+            .put("/data/name")
+        .then()
+            .statusCode(400)
+            .body("error.message", equalTo("Data must only contain primitive values."));
+    }
+
+    @Test
+    public void testWriteSucceeds() {
+        request
+            .accept("application/json")
+            .contentType("application/json")
+            .body("{ \"string\": \"value\", \"boolean\": true, \"number\": 3.142 }")
+            .put("/data/name")
+        .then()
+            .statusCode(200)
+            .body("status", equalTo("Data accepted."));
+
+        Entry entry = dataManager.get("name");
+
+        assertEquals("value", entry.get("string").getRaw());
+        assertEquals(true, entry.get("boolean").getRaw());
+        assertEquals(3.142F, (Double) entry.get("number").getRaw(), 0.001F);
     }
 }
